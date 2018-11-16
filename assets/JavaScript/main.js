fdb55d87f70a4c3b4e3396e4adc6eed08fabd447
matttali09 <matttaliancich09@gmail.com>
2018-11-16 01:04:33 -0500

    still working on getting submit button to register answer choices, but it's styled pretty well now in colors and with media query's

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -1,5 +1,39 @@
 $(document).ready(function () {
+    // globals
+    answersArray = [3, 2, 3, 3, 1, 4, 4, 3, 3];
+    userChoiceArray = [1];
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+    unansweredQuestions = 0;
+
+    $("#submit-button").on("click", function () {
+        for (var i = 1; i < 10; i++) {
+             var userChoices = $("[name='" + i + "']:checked")
+        }
+        if (userChoices != null) {
+            userChoiceArray.push(userChoices.val())
+            console.log(userChoices.val())
+          }
+        
+          console.log(userChoiceArray)
+    })
+
+
+
+
+    // for loop to attach radio buttons to the list items with different names so they can be selected correctly
     for (var i = 1; i < 10; i++) {
-        $('<input type="radio" name="' + i + '" />').prependTo('ul[class=question' + i + '] li');
+        $("<input type='radio' name='" + i + "' />").prependTo('ul[class=question' + i + '] li');
+        for (var j = 1; j < 5; j++) {
+            $("li input").attr("value", j)
+        }
     }
+    // need two for loops in order to apply the right value attribute to each radio button for answer checking(cant without applying 4 to all with this method)
+    // for (var i = 1; i < 10; i++) {
+        
+    //     for (var j = 1; j < 5; j++) {
+    //         $('ul[class=question' + i + '] li input').attr("value", j)
+    //      }
+    // }
+
 });
